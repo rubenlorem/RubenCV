2a5f897ab85d7986375410981670a39d58434c5a
Your Name <you@example.com>
2020-07-13 14:37:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RubenCV
---
+++ js/script.js
@@ -0,0 +1,48 @@
+const burger = document.getElementById('burger'),
+ nav__ul = document.getElementById('nav__ul');
+
+burger.addEventListener('click', () =>{
+ nav__ul.classList.toggle('desplegable');
+ console.log("biennn");
+})
+
+const btn1 = document.getElementById('btn1'),
+ btn2 = document.getElementById('btn2'),
+ btn3 = document.getElementById('btn3'),
+ cv = document.getElementsByClassName('cv'),
+ cor = document.getElementsByClassName('cor'),
+otro = document.getElementsByClassName('otro');
+
+btn1.addEventListener('click',() =>{
+  for(i=0; i < cv.length ; i++){
+      cv[i].classList.remove('seleccionado');
+  }
+  for(i=0; i < cor.length ; i++){
+    cor[i].classList.add('seleccionado');
+}
+for(i=0; i < otro.length ; i++){
+    otro[i].classList.add('seleccionado');
+}
+})
+btn2.addEventListener('click',() =>{
+    for(i=0; i < cor.length ; i++){
+        cor[i].classList.remove('seleccionado');
+    }
+    for(i=0; i < cv.length ; i++){
+        cv[i].classList.add('seleccionado');
+    }
+    for(i=0; i < otro.length ; i++){
+        otro[i].classList.add('seleccionado');
+    }
+})
+btn3.addEventListener('click',() =>{
+    for(i=0; i < otro.length ; i++){
+        otro[i].classList.remove('seleccionado');
+    }
+    for(i=0; i < cv.length ; i++){
+        cv[i].classList.add('seleccionado');
+    }
+    for(i=0; i < cor.length ; i++){
+        cor[i].classList.add('seleccionado');
+    }
+})
