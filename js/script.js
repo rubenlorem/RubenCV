f7d858e59ae41129f589afdd5508255a0f1ce7b9
rubenlorem <68228054+rubenlorem@users.noreply.github.com>
2022-09-24 15:56:28 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -50,50 +50,106 @@ grid6[0].addEventListener('click',() =>{
 })
 
 /*Botones galeia*/
+/*
 const otro = document.getElementsByClassName('otro'),
 cor = document.getElementsByClassName('cor'),
 cv = document.getElementsByClassName('cv');
 
+btn1.addEventListener('click', function() {
+  console.log(getInput())
+});
+btn2.addEventListener('click', function() {
+  console.log(getInput2())
+});
+btn3.addEventListener('click', function() {
+  console.log(getInput3())
+});
 
-btn1.addEventListener('mouseover',() =>{
-    btn1.classList.add('seleccionado');
-    btn2.classList.remove('seleccionado');
-    btn3.classList.remove('seleccionado');
-        for(i=0; i < cor.length ; i++){
-            cor[i].classList.add('oscurecido');
-          }
-        for(i=0; i < cv.length ; i++){
-           cv[i].classList.remove('oscurecido');
-       }
-       for(i=0; i < otro.length ; i++){
-         otro[i].classList.add('oscurecido');
-       }
-})
-btn2.addEventListener('mouseover',() =>{
-    btn2.classList.add('seleccionado');
-    btn1.classList.remove('seleccionado');
-    btn3.classList.remove('seleccionado');
-        for(i=0; i < cor.length ; i++){
-            cor[i].classList.remove('oscurecido');
-          }
-        for(i=0; i < cv.length ; i++){
-           cv[i].classList.add('oscurecido');
-       }
-       for(i=0; i < otro.length ; i++){
-         otro[i].classList.add('oscurecido');
-       }
-})
-btn3.addEventListener('mouseover',() =>{
-    btn3.classList.add('seleccionado');
-    btn1.classList.remove('seleccionado');
-    btn2.classList.remove('seleccionado');
-        for(i=0; i < cor.length ; i++){
-            cor[i].classList.add('oscurecido');
-          }
-        for(i=0; i < cv.length ; i++){
-           cv[i].classList.add('oscurecido');
-       }
-       for(i=0; i < otro.length ; i++){
-         otro[i].classList.remove('oscurecido');
-       }
-})
+function getInput() {
+  btn1.classList.add('seleccionado');
+  btn2.classList.remove('seleccionado');
+  btn3.classList.remove('seleccionado');
+  for(i=0; i < cor.length ; i++){
+    cor[i].classList.add('oscurecido');
+  }
+for(i=0; i < cv.length ; i++){
+   cv[i].classList.remove('oscurecido');
+}
+for(i=0; i < otro.length ; i++){
+ otro[i].classList.add('oscurecido');
+}
+}
+function getInput2() {
+  btn1.classList.remove('seleccionado');
+  btn2.classList.add('seleccionado');
+  btn3.classList.remove('seleccionado');
+  for(i=0; i < cor.length ; i++){
+    cor[i].classList.remove('oscurecido');
+  }
+for(i=0; i < cv.length ; i++){
+   cv[i].classList.add('oscurecido');
+}
+for(i=0; i < otro.length ; i++){
+ otro[i].classList.add('oscurecido');
+}
+}
+function getInput3() {
+  btn1.classList.remove('seleccionado');
+  btn2.classList.remove('seleccionado');
+  btn3.classList.add('seleccionado');
+  for(i=0; i < cor.length ; i++){
+    cor[i].classList.add('oscurecido');
+  }
+for(i=0; i < cv.length ; i++){
+   cv[i].classList.add('oscurecido');
+}
+for(i=0; i < otro.length ; i++){
+ otro[i].classList.remove('oscurecido');
+}
+}*/
+const otro = document.getElementsByClassName('otro'),
+cor = document.getElementsByClassName('cor'),
+cv = document.getElementsByClassName('cv');
+
+btn1.addEventListener('click', function() {
+  btn1.classList.add('seleccionado');
+  btn2.classList.remove('seleccionado');
+  btn3.classList.remove('seleccionado');
+  for(i=0; i < cor.length ; i++){
+    cor[i].classList.add('oscurecido');
+  }
+for(i=0; i < cv.length ; i++){
+   cv[i].classList.remove('oscurecido');
+}
+for(i=0; i < otro.length ; i++){
+ otro[i].classList.add('oscurecido');
+}
+});
+btn2.addEventListener('click', function() {
+  btn1.classList.remove('seleccionado');
+  btn2.classList.add('seleccionado');
+  btn3.classList.remove('seleccionado');
+  for(i=0; i < cor.length ; i++){
+    cor[i].classList.remove('oscurecido');
+  }
+for(i=0; i < cv.length ; i++){
+   cv[i].classList.add('oscurecido');
+}
+for(i=0; i < otro.length ; i++){
+ otro[i].classList.add('oscurecido');
+}
+});
+btn3.addEventListener('click', function() {
+  btn1.classList.remove('seleccionado');
+  btn2.classList.remove('seleccionado');
+  btn3.classList.add('seleccionado');
+  for(i=0; i < cor.length ; i++){
+    cor[i].classList.add('oscurecido');
+  }
+for(i=0; i < cv.length ; i++){
+   cv[i].classList.add('oscurecido');
+}
+for(i=0; i < otro.length ; i++){
+ otro[i].classList.remove('oscurecido');
+}
+});
